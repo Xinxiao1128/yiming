bd44e46d740fd48e295be3d63571609e61e36fdc
Xinxiao1128 <Xinxiao.Li-1@student.uts.edu.au>
2025-05-04 05:47:06 +1000

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-# main.py - Entry point to run the ClearML pipeline
-
 from pipeline_from_tasks import run_pipeline
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("🚀 Starting Agri-Pest-Detection ClearML pipeline...")
+    print_hi('AI Studio')
     run_pipeline()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
